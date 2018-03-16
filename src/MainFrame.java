ff3d7a3d51f275cc08d4abe71caaf195afc0568f
victor <victor@ti2-15>
2018-03-16 15:33:03 +0100

    Fixed trailing zeros after decimal separator.

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,5 @@
 
+import java.math.BigDecimal;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
@@ -72,7 +73,14 @@ public class MainFrame extends javax.swing.JFrame {
     }
     
     private void displayResult() {
-        textFieldDisplay.setText("" + accumulator);
+        String s = ""+accumulator;
+        if (s.contains(".")) {
+            s = s.replaceAll("0+$", "");
+            s = s.replaceAll(".$", "");
+        }
+        textFieldDisplay.setText(s);
+        //BigDecimal number = new BigDecimal(accumulator);
+        //textFieldDisplay.setText(number.stripTrailingZeros().toPlainString());
     }
 
     /**
